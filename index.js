de2100e73641138085b989cc71bfc1f128e9d2e5
kkiyama117 <k.kiyama117@gmail.com>
2023-10-18 17:40:03 +0900

    Keep

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const fsPromises = require("fs/promises");
 
-const hostname = '127.0.0.1';
+const hostname = 'localhost';
 const port = 3000;
 const statusCode = 200;
 
@@ -12,6 +13,36 @@ app.use((_, res, next) => {
 });
 
 app.use(express.static(__dirname + '/public'));
+app.post("/fluent/blob", async (req, res, next) => {
+    // リクエストボディを出力
+    console.log(req.body);
+    // パラメータ名、nameを出力
+    // console.log(req.body.name);
+    try {
+        const rawBody = await new Promise((resolve, reject) => {
+            const chunks = [];
+
+            req.on("data", (chunk) => chunks.push(chunk));
+            req.on("end", () => resolve(Buffer.concat(chunks)));
+            req.on("error", (err) => reject(err));
+        });
+
+        const outputDirectory = path.join(__dirname, "tmp");
+        const outputFilename = Date.now() + req.query.extname;
+        const outputPath = path.join(outputDirectory, outputFilename);
+
+        await fsPromises.mkdir(outputDirectory, {recursive: true});
+        await fsPromises.writeFile(outputPath, rawBody);
+
+        res.status(201).end();
+    } catch (err) {
+        next(err);
+    }
+});
+
+function convert_webm(blob) {
+    ffmpeg()
+}
 
 // app.get("/", () => (req, res) => {
 //     res.sendFile(__dirname + path)
